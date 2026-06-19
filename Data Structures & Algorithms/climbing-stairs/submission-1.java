df83fa0584ce4a2c0280cd1fdbc9681775933bfe
Utkarsh Katiyar <utkarshkatiyar688@gmail.com>
2026-06-19 21:46:56 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        Map<Integer,Integer> map = new HashMap<>();
+        return recursion(n, map);
+    }
+
+    int recursion(int p, Map <Integer, Integer> map) {
+        if(map.containsKey(p)) return map.get(p);
+        else if(p<3) {
+            map.put(p,p); 
+            return p;
+        }
+        else {
+            int sol=recursion(p-1, map) + recursion(p-2, map);
+            map.put(p, sol);
+            return sol;
+        }
+    }
+}
