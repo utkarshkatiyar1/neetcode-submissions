289b1223ecf89a15a551738668e8c8b6a92c6bda
Utkarsh Katiyar <utkarshkatiyar688@gmail.com>
2026-07-02 22:19:46 +0530

    Add: sort-colors - submission-3

---
+++ Data Structures & Algorithms/sort-colors/submission-3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int x=0, y=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0) x++;
+            else if(nums[i]==1) y++;
+        }
+        for(int i=0; i<nums.length; i++){
+            if(i<x) nums[i]=0;
+            else if(i>=x+y) nums[i]=2;
+            else nums[i]=1;
+        }
+    }
+}
